7415f1501a41630b1992d4c7575fab8530ba4fda
nicholasma88 <Nick.Ma@panasonic.aero>
2019-03-12 23:58:34 -0700

    Update lenet.py

--- models/lenet.py
+++ models/lenet.py
@@ -122,7 +122,7 @@ class LeNet:
         return imgs
 
     def predict(self, img):
-        processed = self.color_process(img.item())
+        processed = self.color_process(img.eval())
         return self._model.predict(processed, batch_size=self.batch_size)
     
     def predict_one(self, img):
